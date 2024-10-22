146de250547cbbe59d2e7f3a2cea3e087a1947dc
Derick Sayavong <dsayavong54651@gmail.com>
2024-10-21 22:07:44 -0600

    test2

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -10,6 +10,7 @@ func _on_gate_player_entered_gate(_body) -> void:
 
 func _on_player_laser_signal(pos, direction) -> void:
 	var laser: Area2D = laser_scene.instantiate()
+	print(';asdas')
 	# Update laser position to laser markers
 	laser.position = pos
 	print('hello')
