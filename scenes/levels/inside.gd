9a8666bda92accf5a55d6fb839e3f4f338dd6219
Derick Sayavong <dsayavong54651@gmail.com>
2024-10-31 22:47:58 -0600

    Added transition animation when changing scenes

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -5,4 +5,4 @@ func _on_exit_gate_area_body_entered(_body: Node2D) -> void:
 	var tween: Tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.5)
 	#get_tree().change_scene_to_file("res://scenes/levels/outside.tscn")
-	get_tree().change_scene_to_packed.call_deferred(outside_level_scene)
+	TransitionLayer.change_scene("res://scenes/levels/outside.tscn")
